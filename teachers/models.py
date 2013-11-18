fc63e4f29d29ada616f213083dd753e1e797e919
Nonni <levito@gmail.com>
2013-11-18 10:48:21 +0100

    more fields

--- teachers/models.py
+++ teachers/models.py
@@ -21,7 +21,7 @@ class Diploma(models.Model):
 
     def __str__(self):
         return self.name
-        
+         
 class Teacher(models.Model):
     GENDER_CHOICES = (
         (0, 'Karl'),
@@ -35,6 +35,11 @@ class Teacher(models.Model):
     dob = models.DateField('Fæðingarár')
     deceased = models.DateField('Dánarár')
     birthplace = models.ForeignKey(Area)
+    children_related = models.IntegerField('Fjöldi skyldra barna', blank=True)
+    children_foster = models.IntegerField('Fjöldi fósturbarna', blank=True)
+    children_adopted = models.IntegerField('Fjöldi kjörbarna', blank=True)
+    dob_youngest_child = models.DateField('Fæðingarár yngsta barns', blank=True)
+    dob_oldest_child = models.DateField('Fæðingarár elsta barns', blank=True)
     
     def __str__(self):
         return self.name
@@ -56,4 +61,16 @@ class Education(models.Model):
     teacher = models.ForeignKey(Teacher)
     year = models.DateField('Próftökuár')
     diploma = models.ForeignKey(Diploma)
+
+class Marriage(models.Model):
+    MARRIAGE_CHOICES = (
+        (1, 'Ógift/ur'),
+        (3, 'Gift/ur'),
+        (4, 'Ekkill/Ekkja'),
+        (6, 'Fráskilin/n'),
+    )
+    
+    teacher = models.ForeignKey(Teacher)
+    marriage_status = models.IntegerField('Hjúskapur', choices=MARRIAGE_CHOICES, default=1)
+    year = models.DateField('Hjúskaparár')
     
